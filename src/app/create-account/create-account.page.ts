021bbfbb045c04d022da4dded14aacaf0efbf886
fellipe anchieta <fellipeanchieta@gmail.com>
2018-08-27 18:13:34 -0300

    criar conta

--- src/app/create-account/create-account.page.ts
+++ src/app/create-account/create-account.page.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { AngularFireAuth } from 'angularfire2/auth';
 
 @Component({
   selector: 'app-create-account',
   templateUrl: './create-account.page.html',
   styleUrls: ['./create-account.page.scss'],
 })
-export class CreateAccountPage implements OnInit {
+export class CreateAccountPage {
 
-  constructor() { }
+  criarContaForm = new FormGroup({
+    email: new FormControl(''),
+    senha: new FormControl('')
+  });
 
-  ngOnInit() {
+  constructor(public fireAuth: AngularFireAuth, public router: Router) { }
+
+  onCriarConta() {
+    this.fireAuth.auth.createUserWithEmailAndPassword(this.email, this.senha)
+      .then(() => {
+        this.router.navigate(['/home']);
+      });
   }
 
+  get email() { return this.criarContaForm.get('email').value; }
+  get senha() { return this.criarContaForm.get('senha').value; }
+
 }
+
